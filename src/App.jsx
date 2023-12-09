2655e13a68131fa21b1f44b17d642178d0701da9
shubhadamohite <shubhada.rocks11@gmail.com>
2023-12-08 22:16:21 -0500

    resolved dependency with fetchimages and page var, added error msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import "./index.css";
 import axios from "axios";
 import { useState } from "react";
-import { useEffect } from "react";
-// import image from 'plotly.js/lib/image';
+import { useCallback, useEffect } from "react";
 
 const API_URL = "https://api.unsplash.com/search/photos";
 const IMAGES_PER_PAGE = 20;
@@ -14,6 +14,7 @@ const App = () => {
   const [images, setImages] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const resetSearch = () => {
     setPage(1);
@@ -30,27 +31,39 @@ const App = () => {
     resetSearch();
   };
   console.log("Page", page);
-
-  const fetchImages = async () => {
+  {
+    /**
+By using useCallback with [page] as a dependency, you ensure that the fetchImages function is recreated only when the page variable changes. 
+Then, in the useEffect, you specify [fetchImages] as a dependency, so the effect will run whenever the fetchImages function changes.
+This setup satisfies the exhaustive-deps rule of ESLint, ensuring that all dependencies are included.
+*/
+  }
+  const fetchImages = useCallback(async () => {
     try {
-      const { data } = await axios.get(
-        `${API_URL}?query=${
-          searchInput.current.value
-        }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
-      console.log("data", data);
-      setImages(data.results);
-      setTotalPages(data.total_pages);
+      if (searchInput.current.value) {
+        setLoading(true);
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        console.log("data", data);
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+        setLoading(false);
+      }
     } catch (error) {
       console.log(error);
+      console.loading(false);
     }
-  };
+  }, [page]);
 
   useEffect(() => {
     fetchImages();
-  }, [page]);
+  }, [fetchImages]);
+
   return (
     <div className="container">
       <h1 className="title">Image Search</h1>
@@ -70,16 +83,30 @@ const App = () => {
         <div onClick={() => handleSelection("cats")}>Cats</div>
         <div onClick={() => handleSelection("shoes")}>Shoes</div>
       </div>
-      <div className="images">
-        {images.map((image) => (
-          <img
-            key={image.id}
-            src={image.urls.small}
-            alt={image.alt_description}
-            className="image"
-          />
-        ))}
-      </div>
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <>
+          <div className="images">
+            {images.map((image) => (
+              <img
+                key={image.id}
+                src={image.urls.small}
+                alt={image.alt_description}
+                className="image"
+              />
+            ))}
+          </div>
+          <div className="buttons">
+            {page > 1 && (
+              <Button onClick={() => setPage(page - 1)}>Previous</Button>
+            )}
+            {page < totalPages && (
+              <Button onClick={() => setPage(page + 1)}>Next</Button>
+            )}
+          </div>
+        </>
+      )}
       <div className="buttons">
         {page > 1 && (
           <Button onClick={() => setPage(page - 1)}>Previous</Button>
